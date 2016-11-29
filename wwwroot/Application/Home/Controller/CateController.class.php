2ae765f20bb7093cce01bfa8980fd2a91d3db8d0
Home_zou <xzhb_1985@126.com>
2016-11-30 00:18:57 +0800

    页面整理....

--- wwwroot/Application/Home/Controller/CateController.class.php
+++ wwwroot/Application/Home/Controller/CateController.class.php
@@ -13,7 +13,7 @@ namespace Home\Controller;
  * 前台首页控制器
  * 主要获取首页聚合数据
  */
-class ArticleController extends HomeController {
+class CateController extends HomeController {
 
 	//系统首页
     public function index(){
