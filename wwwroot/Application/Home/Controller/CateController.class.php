06e00eb9241f2cfebb75a86e4dfdcdd4b565407a
Home_zou <xzhb_1985@126.com>
2016-12-05 00:20:48 +0800

    文章页功能添加

--- wwwroot/Application/Home/Controller/CateController.class.php
+++ wwwroot/Application/Home/Controller/CateController.class.php
@@ -10,15 +10,42 @@
 namespace Home\Controller;
 
 /**
- * 前台首页控制器
- * 主要获取首页聚合数据
+ * 文档模型控制器
+ * 文档模型列表和详情
  */
 class CateController extends HomeController {
+   	/* 文档模型频道页 */
+	public function index(){
+		/* 分类信息 */
+		$category = $this->category();
+		//频道页只显示模板，默认不读取任何内容
+		//内容可以通过模板标签自行定制
+		/* 模板赋值并渲染模板 */
+		$this->assign('category', $category);
+		$this->display($category['template_index']);
+	}
+	/* 文档分类检测 */
+	private function category($id = 0){
+		/* 标识正确性检测 */
+		$id = $id ? $id : I('get.category', 0);
+		if(empty($id)){
+			$this->error('没有指定文档分类！');
+		}
 
-	//系统首页
-    public function index(){
-               
-        $this->display();
-    }
+		/* 获取分类信息 */
+		$category = D('Category')->info($id);
+		if($category && 1 == $category['status']){
+			switch ($category['display']) {
+				case 0:
+					$this->error('该分类禁止显示！');
+					break;
+				//TODO: 更多分类显示状态判断
+				default:
+					return $category;
+			}
+		} else {
+			$this->error('分类不存在或被禁用！');
+		}
+	}
 
 }
